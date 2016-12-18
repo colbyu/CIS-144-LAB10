b2bf6b29aca51e214a34b45f7f60c1657d633710
colby <colby@10.1.10.3>
2016-12-18 16:45:52 -0600

    Made copy-paste modifications as requested

--- Lab10/src/lab10/GiftCards.java
+++ Lab10/src/lab10/GiftCards.java
@@ -1,157 +1,70 @@
-/*
-
- * To change this license header, choose License Headers in Project Properties.
-
- * To change this template file, choose Tools | Templates
-
- * and open the template in the editor.
-
- */
-
 package lab10;
 
- 
-
 import java.text.DateFormat;
-
 import java.text.NumberFormat;
-
 import java.text.SimpleDateFormat;
-
 import java.util.Date;
 
- 
-
- 
-
-//Janet Leu
+//Colby Undeerhill
 
 public class GiftCards
-
-{
-
- static NumberFormat nf = NumberFormat.getCurrencyInstance();
-
-  DateFormat dateForm = new SimpleDateFormat("MM/dd/yyyy");
-
-  Date today = new Date();
-
-   
-
-  // define the class data members
-
-  public boolean expires;
-
-  private double balance;
-
-  public int cardNum;
-
-  private char cardType;
-
-  public String issueDate;
-
-  private String holder;
-
- 
-
-  // define the member methods
-
-  // default constructor
-
-  public GiftCards() {}
-
-  // overloaded constructor
-
-  public GiftCards(String n, double amt)
-
-  {
-
-    holder = n;
-
-    balance = amt;
-
-      }
-
- 
-
-  public void IssueGiftCard()
-
-  {
-
- System.out.println("");
-
- System.out.println(" [ Card Issued ] ");
-
- System.out.println(" Today's Date is: " + dateForm.format(today));
-
- System.out.println(" Card Holder . . . " + holder);
-
- System.out.println(" Card Number . . . " + cardNum);
-
- System.out.println(" Card Amount . . . " + 
-
-                 NumberFormat.getCurrencyInstance().format(balance));
-
-  }
-
-public double getBalance()
-
-  {
-
-    return balance;
-
-  }
-
-  public void setBalance(double b)
-
-  {
-
-    balance = b;
-
-  }
-
-  public String getHolder()
-
-  {
-
-    return holder;
-
-  }
-
-  public void setHolder(String h)
-
-  {
-
-    holder = h;
-
-  }
-
-   
-
-  public void printCurrentGiftCardInfo(double spend)
-
-  {
-
-  System.out.println("");
-
-  System.out.println("");
-
-  System.out.println(" [ Card Balance ]");
-
-  System.out.println(" Today's Date is: " + dateForm.format(today));
-
-  System.out.println(" Deducted  . . . . " +
-
-                  NumberFormat.getCurrencyInstance().format(spend));
-
- System.out.println(" Card Amount . . . " +
-
-                  NumberFormat.getCurrencyInstance().format(balance - spend));
-
-                   
-
-  }
-
-}
+    {
+    static NumberFormat nf = NumberFormat.getCurrencyInstance();
+    DateFormat dateForm = new SimpleDateFormat("MM/dd/yyyy");
+    Date today = new Date();
+    // define the class data members
+    public boolean expires;
+    private double balance;
+    public int cardNum;
+    private char cardType;
+    public String issueDate;
+    private String holder;
+    // define the member methods
+    // default constructor
+    public GiftCards() {}
+    // overloaded constructor
+    public GiftCards(String n, double amt)
+        {
+        holder = n;
+        balance = amt;
+        }
+    public void IssueGiftCard()
+        {
+        System.out.println("");
+        System.out.println(" [ Card Issued ] ");
+        System.out.println(" Today's Date is: " + dateForm.format(today));
+        System.out.println(" Card Holder . . . " + holder);
+        System.out.println(" Card Number . . . " + cardNum);
+        System.out.println(" Card Amount . . . " + 
+            NumberFormat.getCurrencyInstance().format(balance));
+        }
+    public double getBalance()
+        {
+        return balance;
+        }
+    public void setBalance(double b)
+        {
+        balance = b;
+        }
+        public String getHolder()
+        {
+        return holder;
+        }
+    public void setHolder(String h)
+        {
+        holder = h;
+        }
+    public void printCurrentGiftCardInfo(double spend)
+        {
+        System.out.println("");
+        System.out.println("");
+        System.out.println(" [ Card Balance ]");
+        System.out.println(" Today's Date is: " + dateForm.format(today));
+        System.out.println(" Deducted  . . . . " +
+            NumberFormat.getCurrencyInstance().format(spend));
+        System.out.println(" Card Amount . . . " +
+            NumberFormat.getCurrencyInstance().format(balance - spend));
+        }
+    }
 
  
--- Lab10/src/lab10/TestGiftCards.java
+++ Lab10/src/lab10/TestGiftCards.java
@@ -1,93 +1,35 @@
-/*
-
- * To change this license header, choose License Headers in Project Properties.
-
- * To change this template file, choose Tools | Templates
-
- * and open the template in the editor.
-
- */
-
 package lab10;
 
- 
-
 import javax.swing.JOptionPane;
+//Colby Underhill
 
 public class TestGiftCards
-
-//Janet Leu
-
-{
-
-public static void main(String[] args)
-
-{
-
-GiftCards gc = new GiftCards("", 0.0);
-
- 
-
-// display opening message
-
-String message = "welcome";
-
-JOptionPane.showMessageDialog(null, "Message: " + message,
-
-"Message", JOptionPane.PLAIN_MESSAGE);
-
-                                                                                                               
-
-// set the gift card holder's name
-
-String str =
-
-JOptionPane.showInputDialog(null, "gift card holder's name : ");
-
-gc.setHolder(str);
-
-                                                                                                               
-
-// obtain the gift card number
-
-str = JOptionPane.showInputDialog(null, "gift card number : ");
-
-gc.cardNum = Integer.parseInt(str);
-
-                                                                                                               
-
-// set the gift card amount
-
-str = JOptionPane.showInputDialog(null, "gift card amount : ");
-
-gc.setBalance(Double.parseDouble(str));
-
-                                                                                                               
-
-// issue the gift card
-
-gc.IssueGiftCard();
-
-                                                                                                               
-
-// use the gift card
-
-double useCard = 0;
-
-str =
-
-JOptionPane.showInputDialog(null, "deduct amount from card : ");
-
-useCard = Double.parseDouble(str);
-
-                        
-
- // print current card balance
-
-gc.printCurrentGiftCardInfo(useCard);
-
- 
-
-     }
-
-}
+    {
+    public static void main(String[] args)
+        {
+        GiftCards gc = new GiftCards("", 0.0);
+        // display opening message
+        String message = "welcome";
+        JOptionPane.showMessageDialog(null, "Message: " + message,
+        "Message", JOptionPane.PLAIN_MESSAGE);
+        // set the gift card holder's name
+        String str =
+            JOptionPane.showInputDialog(null, "gift card holder's name : ");
+        gc.setHolder(str);
+        // obtain the gift card number
+        str = JOptionPane.showInputDialog(null, "gift card number : ");
+        gc.cardNum = Integer.parseInt(str);
+        // set the gift card amount
+        str = JOptionPane.showInputDialog(null, "gift card amount : ");
+        gc.setBalance(Double.parseDouble(str));
+        // issue the gift card
+        gc.IssueGiftCard();
+        // use the gift card
+        double useCard = 0;
+        str =
+            JOptionPane.showInputDialog(null, "deduct amount from card : ");
+        useCard = Double.parseDouble(str);
+        // print current card balance
+        gc.printCurrentGiftCardInfo(useCard);
+        }
+    }

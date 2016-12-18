3fd02d4924fb31a74a990c29c6cf979d4c6a0999
colby <colby@10.1.10.3>
2016-12-18 16:33:56 -0600

    Made copy-paste modifications as requested

--- Lab10/src/lab10/GiftCards.java
+++ Lab10/src/lab10/GiftCards.java
@@ -1,73 +1,157 @@
+/*
+
+ * To change this license header, choose License Headers in Project Properties.
+
+ * To change this template file, choose Tools | Templates
+
+ * and open the template in the editor.
+
+ */
+
 package lab10;
 
+ 
+
 import java.text.DateFormat;
+
 import java.text.NumberFormat;
+
 import java.text.SimpleDateFormat;
+
 import java.util.Date;
-//Colby Underhill
+
+ 
+
+ 
+
+//Janet Leu
 
 public class GiftCards
+
 {
 
-    static NumberFormat nf = NumberFormat.getCurrencyInstance();
-    DateFormat dateForm = new SimpleDateFormat("MM/dd/yyyy");
-    Date today = new Date();
+ static NumberFormat nf = NumberFormat.getCurrencyInstance();
 
-    // define the class data members 
-    public boolean expires;
-    private double balance;
-    public int cardNum;
-    private char cardType;
-    public String issueDate;
-    private String holder;
+  DateFormat dateForm = new SimpleDateFormat("MM/dd/yyyy");
 
-    // define the member methods
-    // default constructor
-    public GiftCards()
-    {
-    }
-    // overloaded constructor
+  Date today = new Date();
 
-    public GiftCards(String n, double amt)
-    {
-        holder = n;
-        balance = amt;
-    }
+   
 
-    public void IssueGiftCard()
-    {
-        System.out.println("");
-        System.out.println(" Card Issued");
-        System.out.println(" Today's Date is: " + dateForm.format(today));
-        System.out.println(" Card Holder . . . " + holder);
-        System.out.println(" Card Amount . . . " + balance);
-    }
+  // define the class data members
 
-    public double getBalance()
-    {
-        return balance;
-    }
+  public boolean expires;
 
-    public void setBalance(double b)
-    {
-        balance = b;
-    }
+  private double balance;
 
-    public String getHolder()
-    {
-        return holder;
-    }
+  public int cardNum;
 
-    public void setHolder(String h)
-    {
-        holder = h;
-    }
+  private char cardType;
 
-    public void printCurrentGiftCardInfo(double spend)
-    {
-        System.out.println("");
-        System.out.println(" Card Balance");
-        System.out.println(" Today's Date is: " + dateForm.format(today));
-        System.out.println(" Card Amount . . . " + balance);
-    }
+  public String issueDate;
+
+  private String holder;
+
+ 
+
+  // define the member methods
+
+  // default constructor
+
+  public GiftCards() {}
+
+  // overloaded constructor
+
+  public GiftCards(String n, double amt)
+
+  {
+
+    holder = n;
+
+    balance = amt;
+
+      }
+
+ 
+
+  public void IssueGiftCard()
+
+  {
+
+ System.out.println("");
+
+ System.out.println(" [ Card Issued ] ");
+
+ System.out.println(" Today's Date is: " + dateForm.format(today));
+
+ System.out.println(" Card Holder . . . " + holder);
+
+ System.out.println(" Card Number . . . " + cardNum);
+
+ System.out.println(" Card Amount . . . " + 
+
+                 NumberFormat.getCurrencyInstance().format(balance));
+
+  }
+
+public double getBalance()
+
+  {
+
+    return balance;
+
+  }
+
+  public void setBalance(double b)
+
+  {
+
+    balance = b;
+
+  }
+
+  public String getHolder()
+
+  {
+
+    return holder;
+
+  }
+
+  public void setHolder(String h)
+
+  {
+
+    holder = h;
+
+  }
+
+   
+
+  public void printCurrentGiftCardInfo(double spend)
+
+  {
+
+  System.out.println("");
+
+  System.out.println("");
+
+  System.out.println(" [ Card Balance ]");
+
+  System.out.println(" Today's Date is: " + dateForm.format(today));
+
+  System.out.println(" Deducted  . . . . " +
+
+                  NumberFormat.getCurrencyInstance().format(spend));
+
+ System.out.println(" Card Amount . . . " +
+
+                  NumberFormat.getCurrencyInstance().format(balance - spend));
+
+                   
+
+  }
+
 }
+
+ 
